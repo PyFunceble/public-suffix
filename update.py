d597b6fe9612e0f5db72895f49e8394d8d7f5a2d
funilrys <contact@funilrys.com>
2019-12-11 22:01:13 +0100

    Introduction of the base of the repository.

---
+++ update.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import PyFunceble
+import colorama
+from PyFunceble.engine import ci
+
+if __name__ == "__main__":
+
+    colorama.init(autoreset=True)
+    
+    # We load our custom configuration
+    CUSTOM_CONFIG = {
+        "ci": True,
+        "ci_autosave_final_commit": "Update iana-domains-db.json",
+        "multiprocess": True,
+        "maximal_processes": 50,
+        "dns_server": [
+            "one.one.one.one"
+        ]
+    }
+    PyFunceble.load_config(custom=CUSTOM_CONFIG)
+    
+    # We initiate the repostiory.
+    ci_engine = ci.TravisCI()
+    ci_engine.init() 
+
+    # We process the update of the file.
+    PyFunceble.lookup.PublicSuffix().update()
+
+    # We commit the new state.
+    ci_engine.end_commit()
